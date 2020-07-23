95055afb1fa0a8653d0396dd2d74a200f8c07f4a
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 17:05:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end   
 
   def patients 
-    self.appointments.map {|appointment| appointment.patient}
+    appointments.self.map {|appointment| appointment.patient}
   end   
   
   def self.all 
