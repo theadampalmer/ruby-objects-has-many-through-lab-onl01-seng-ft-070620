1a38528789134594926fea68e9845ccd4fa8564a
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 17:31:47 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def doctors
-    appointments.map{|appointment| appointment.doctor}
+    appointments.map{|appointment| appointment.doctors}
     # Appointment.all.select{|appointment| appointment.patient == self}.map{|item| item.doctor}
   end   
   
