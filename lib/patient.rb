235f7dac8b1de79e452779afe289705f0bdcbd94
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 17:11:00 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
   end  
 
   def new_appointment(doctor, date)
-    appointment = Appointment.new(date, self, doctor)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
